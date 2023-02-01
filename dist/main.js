740673ca20ff552aeff5cd967499294696b39416
Joohyun Kim <corinthionia@gmail.com>
2023-02-01 21:51:20 +0900

    Initial setting

---
+++ dist/main.js
@@ -0,0 +1,19 @@
+/******/ (function() { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ([
+/* 0 */
+/***/ (function() {
+
+throw new Error("Module parse failed: Unexpected token (4:16)\nYou may need an appropriate loader to handle this file type, currently no loaders are configured to process this file. See https://webpack.js.org/concepts#loaders\n| import App from './App';\n| \n> ReactDOM.render(<App />, document.getElementById('root'));\n| ");
+
+/***/ })
+/******/ 	]);
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module doesn't tell about it's top-level declarations so it can't be inlined
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__[0]();
+/******/ 	
+/******/ })()
+;
