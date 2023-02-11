b850f29d27f23ea171c87bed98015c281b3d7123
Joohyun Kim <corinthionia@gmail.com>
2023-02-11 23:42:05 +0900

    chore: initial setting

---
+++ commitlint.config.ts
@@ -0,0 +1,13 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      ['feat', 'fix', 'hotfix', 'refactor', 'docs', 'style', 'design', 'chore'],
+    ],
+    'type-case': [2, 'always', 'lower-case'],
+  },
+};
+
+export {};
