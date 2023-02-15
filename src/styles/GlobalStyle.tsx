5144e7ae2f048bdd18f3a77a2dcb9948d1a91cf1
Joohyun Kim <corinthionia@gmail.com>
2023-02-15 19:51:45 +0900

    feat: 폰트 추가 및 적용

--- src/styles/GlobalStyle.tsx
+++ src/styles/GlobalStyle.tsx
@@ -1,17 +1,17 @@
 import { Global, css } from '@emotion/react';
 import 'normalize.css';
+import './font.css';
 
 const defaultStyle = css`
   * {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
-  }
-
-  html {
     font-family: 'Pretendard', Arial, Helvetica, sans-serif;
+    font-display: fallback;
   }
 
+  html,
   body {
     margin: 0;
     padding: 0;
