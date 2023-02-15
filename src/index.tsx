66cdb4fc5224c12c016dee2da0e37a038103b09a
Joohyun Kim <corinthionia@gmail.com>
2023-02-15 16:15:56 +0900

    feat: 디자인 시스템 적용을 위한 theme 추가

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,16 @@
-import React from 'react';
+import { ThemeProvider } from '@emotion/react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import GlobalStyle from './styles/GlobalStyle';
+import theme from './styles/theme';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) throw new Error('Failed to find the root element');
 const root = ReactDOM.createRoot(rootElement);
 
 root.render(
-  <>
+  <ThemeProvider theme={theme}>
     <GlobalStyle />
     <App />
-  </>
+  </ThemeProvider>
 );
