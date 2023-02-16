3bf18f820a4c81fe58eb792420a0084316169244
Joohyun Kim <corinthionia@gmail.com>
2023-02-16 20:58:21 +0900

    feat: 페이지 라우팅을 위한 react-router-dom 세팅 (#6)
    * feat: Global Style 추가 및 적용
    
    * feat: 디자인 시스템 적용을 위한 theme 추가
    
    * feat: 폰트 추가 및 적용
    
    * feat: react-router-dom 세팅

--- src/index.tsx
+++ src/index.tsx
@@ -4,13 +4,19 @@ import App from './App';
 import GlobalStyle from './styles/GlobalStyle';
 import theme from './styles/theme';
 
+import { BrowserRouter } from 'react-router-dom';
+import GlobalStyle from './styles/GlobalStyle';
+import theme from './styles/theme';
+
 const rootElement = document.getElementById('root');
 if (!rootElement) throw new Error('Failed to find the root element');
 const root = ReactDOM.createRoot(rootElement);
 
 root.render(
   <ThemeProvider theme={theme}>
-    <GlobalStyle />
-    <App />
+    <BrowserRouter>
+      <GlobalStyle />
+      <App />
+    </BrowserRouter>
   </ThemeProvider>
 );
