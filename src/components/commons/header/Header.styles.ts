4be9f7169e6d112baedfdc7585ccb866db0219d3
Joohyun Kim <corinthionia@gmail.com>
2023-02-17 23:52:54 +0900

    design: 세부 디자인 적용

--- src/components/commons/header/Header.styles.ts
+++ src/components/commons/header/Header.styles.ts
@@ -12,16 +12,27 @@ export const Wrapper = styled.div`
 `;
 
 export const Title = styled.div`
+  position: absolute;
+
   width: 228px;
+
   margin: 0 auto;
   text-align: center;
+
+  display: -webkit-box;
+  -webkit-line-clamp: 1;
+  -webkit-box-orient: vertical;
+  overflow: hidden;
+  text-overflow: hidden;
 
   color: ${theme.colors.gray7};
   ${theme.typography.system_2_semibold};
 `;
 
 export const ShareIcon = styled.img`
+  z-index: 2;
+
   width: 24px;
   height: 24px;
-  margin-right: 8px;
+  margin: 0 28px 0 auto;
 `;
--- src/pages/Current.tsx
+++ src/pages/Current.tsx
@@ -11,17 +11,22 @@ const Current = () => {
   return (
     <Wrapper>
       <Header title={room.title} />
-      <Timer />
-      <Title>실시간 참여 현황</Title>
-      <ProgressBar
-        headCount={room.headCount}
-        participants={room.participants}
-      />
-      <Subtitle>참여하지 않은 친구들에게 메시지를 보내보세요!</Subtitle>
-      {room.participants.map((participant) => (
-        <ParticipantsBlock participant={participant} />
-      ))}
-      <Title>실시간 조율 현황</Title>
+      <Body>
+        <Timer />
+        <Title>실시간 참여 현황</Title>
+        <Subtitle>참여하지 않은 친구들에게 메시지를 보내보세요!</Subtitle>
+        <Proportion>1/7</Proportion>
+        <ProgressBar
+          headCount={room.headCount}
+          participants={room.participants}
+        />
+        <Participants>
+          {room.participants.map((participant) => (
+            <ParticipantsBlock participant={participant} />
+          ))}
+        </Participants>
+        <Title>실시간 조율 현황</Title>
+      </Body>
     </Wrapper>
   );
 };
@@ -31,22 +36,51 @@ const Wrapper = styled.div`
   height: 812px;
   margin: 0 auto;
   margin-top: 30px;
+
+  border: 1px solid grey;
+`;
+
+const Body = styled.div`
+  width: 100%;
+  height: 100%;
   padding: 0 20px;
+  margin-top: 32px;
 
   display: flex;
   flex-direction: column;
-  border: 1px solid grey;
-  overflow: auto;
 `;
 
 const Title = styled.div`
+  margin-top: 32px;
+
   color: ${theme.colors.gray7};
   ${theme.typography.system_1_semibold};
 `;
 
 const Subtitle = styled.div`
+  margin-top: 6px;
+
   color: ${theme.colors.gray5};
   ${theme.typography.system_1_regular};
+`;
+
+const Proportion = styled.span`
+  margin-top: 42px;
+  margin-bottom: 4px;
+  margin-left: auto;
+
+  color: ${theme.colors.gray5};
+  ${theme.typography.system_2_medium};
+`;
+
+const Participants = styled.div`
+  width: 100%;
+  margin-top: 16px;
+
+  display: flex;
+  flex-wrap: wrap;
+  row-gap: 6px;
+  column-gap: 5px;
 `;
 
 export default Current;
