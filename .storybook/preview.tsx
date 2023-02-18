0b494422012a63c413116431d4ee2ec046a450da
Joohyun Kim <corinthionia@gmail.com>
2023-02-18 17:10:14 +0900

    chore: 스토리북에 전역 스타일링 적용 (#10)

---
+++ .storybook/preview.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import GlobalStyle from '../src/styles/GlobalStyle';
+
+export const decorators = [
+  (Story) => (
+    <>
+      <GlobalStyle />
+      <Story />
+    </>
+  ),
+];
+
+export const parameters = {
+  actions: { argTypesRegex: '^on[A-Z].*' },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+};
