8f2e7f6e690831fb7475ccec421e12984d5b4e73
shgus1224 <shgus1224@naver.com>
2023-02-19 13:17:54 +0900

    feat: 시간 설정 기능 추가 및 CSS 파일 분리

---
+++ src/components/TimePicker.tsx
@@ -0,0 +1,89 @@
+import { useState } from 'react';
+import '../styles/slick.css';
+import Slider from 'react-slick';
+import styled from '@emotion/styled';
+import {
+  MainContainer,
+  StyledSlider,
+  TextContainer,
+  TimeText,
+} from '../styles/TimePicker.styles';
+
+const Current = () => {
+  const TIME_ARRAY = [
+    '09:00',
+    '10:00',
+    '11:00',
+    '12:00',
+    '13:00',
+    '14:00',
+    '15:00',
+    '16:00',
+    '17:00',
+    '18:00',
+    '19:00',
+    '20:00',
+    '21:00',
+    '22:00',
+    '23:00',
+    '00:00',
+    '01:00',
+    '02:00',
+    '03:00',
+    '04:00',
+    '05:00',
+    '06:00',
+    '07:00',
+    '08:00',
+  ];
+
+  const settings = (startEnd: string) => {
+    const setting = {
+      dots: false,
+      infinite: true,
+      speed: 200,
+      slidesToShow: 3,
+      slidesToScroll: 3,
+      swipeToSlide: true,
+      centerMode: true,
+      arrows: false,
+      vertical: true,
+      swipe: true,
+      verticalSwiping: true,
+      useTransform: false,
+      centerPadding: '0px',
+      touchThreshold: 100,
+      cssEase: 'linear',
+      focusOnSelect: true,
+    };
+
+    return setting;
+  };
+
+  return (
+    <MainContainer>
+      <StyledSlider {...settings('start')}>
+        {TIME_ARRAY.map((time: string) => {
+          return (
+            <div>
+              <TimeText>{time}</TimeText>
+            </div>
+          );
+        })}
+      </StyledSlider>
+      <TextContainer>부터</TextContainer>
+      <StyledSlider {...settings('end')}>
+        {TIME_ARRAY.map((time: string) => {
+          return (
+            <div>
+              <TimeText>{time}</TimeText>
+            </div>
+          );
+        })}
+      </StyledSlider>
+      <TextContainer>까지</TextContainer>
+    </MainContainer>
+  );
+};
+
+export default Current;
