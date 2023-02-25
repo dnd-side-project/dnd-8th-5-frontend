65d0343c1e473ee6eb598208517b8432afc64342
Hyun <81014501+YesHyeon@users.noreply.github.com>
2023-02-25 17:51:49 +0900

    feat: 방 생성 페이지 달력 컴포넌트 구현 (#13)
    * feat: 방 생성 페이지 달력 컴포넌트 추가
    
    * style: react-multi-date-picker 라이브러리 CSS 요소 오버라이딩
    
    * refactor: className 변경
    
    * feat: 날짜선택 방법을 스위치하는 토글 기능 추가
    
    * style: 달력 컴포넌트 CSS 수정
    
    * fix: 토글키 순서 변경
    
    * fix: Calendar CSS 파일 분리 및 setDateArray 방법 수정
    
    * fix: 토글키 컴포넌트 분리 및 CSS 수정
    
    * chore: 토글키 컴포넌트화 진행후 불필요한 함수 삭제
    
    * chore: 토글 컴포넌트화로 불필요한 CSS 요소 삭제

---
+++ src/components/Toggle.tsx
@@ -0,0 +1,30 @@
+import { useState, useCallback } from 'react';
+
+import { ToggleBtn, ToggleText, Circle } from '../styles/toggle.styles';
+
+interface ToggleProps {
+  text: Array<string>;
+  toggle: boolean;
+  setData: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+const Toggle = (props: ToggleProps) => {
+  const [isToggle, setIsToggle] = useState(props.toggle);
+
+  const clickedToggle = useCallback(() => {
+    props.setData((prev) => !prev);
+    setIsToggle((prev) => !prev);
+  }, [isToggle]);
+
+  return (
+    <ToggleBtn onClick={clickedToggle} toggle={isToggle}>
+      <ToggleText>{props.text[0]}</ToggleText>
+      <ToggleText>{props.text[1]}</ToggleText>
+      <Circle toggle={isToggle}>
+        {isToggle ? props.text[0] : props.text[1]}
+      </Circle>
+    </ToggleBtn>
+  );
+};
+
+export default Toggle;
