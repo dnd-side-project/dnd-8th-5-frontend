1a28aef9c1f30a88fbef97665baed25f04f8e389
shgus1224 <shgus1224@naver.com>
2023-03-02 02:43:22 +0900

    feat: 방생성 정보를 위한 Recoil 추가

--- src/index.tsx
+++ src/index.tsx
@@ -5,16 +5,19 @@ import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import GlobalStyle from './styles/GlobalStyle';
 import theme from './styles/theme';
+import { RecoilRoot } from 'recoil';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) throw new Error('Failed to find the root element');
 const root = ReactDOM.createRoot(rootElement);
 
 root.render(
-  <ThemeProvider theme={theme}>
-    <BrowserRouter>
-      <GlobalStyle />
-      <App />
-    </BrowserRouter>
-  </ThemeProvider>
+  <RecoilRoot>
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <GlobalStyle />
+        <App />
+      </BrowserRouter>
+    </ThemeProvider>
+  </RecoilRoot>
 );
