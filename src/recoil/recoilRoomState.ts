474823c7e13d03b32d24db178b4de8a98d713b90
shgus1224 <shgus1224@naver.com>
2023-03-02 23:08:30 +0900

    feat: recoil 전역변수 구현 및 적용

--- src/recoil/recoilRoomState.ts
+++ src/recoil/recoilRoomState.ts
@@ -1,6 +1,14 @@
-import { atom } from 'recoil';
+import { atom, selector } from 'recoil';
 
 export const recoilRoomState = atom({
   key: 'recoilRoomState',
   default: {},
+});
+
+export const recoilRoomInfoState = selector({
+  key: 'recoilRoomInfoState', // 고유한 키 값
+  get: ({ get }) => {
+    const roomInfo = get(recoilRoomState);
+    return roomInfo;
+  },
 });
