b4f0f606525a9e482af9e6dc599a052141b271aa
shgus1224 <shgus1224@naver.com>
2023-03-02 23:11:28 +0900

    feat: react-query 기능추가

--- src/index.tsx
+++ src/index.tsx
@@ -7,17 +7,24 @@ import GlobalStyle from './styles/GlobalStyle';
 import theme from './styles/theme';
 import { RecoilRoot } from 'recoil';
 
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
+
 const rootElement = document.getElementById('root');
 if (!rootElement) throw new Error('Failed to find the root element');
 const root = ReactDOM.createRoot(rootElement);
 
+const queryClient = new QueryClient();
+
 root.render(
-  <RecoilRoot>
-    <ThemeProvider theme={theme}>
-      <BrowserRouter>
-        <GlobalStyle />
-        <App />
-      </BrowserRouter>
-    </ThemeProvider>
-  </RecoilRoot>
+  <QueryClientProvider client={queryClient}>
+    <RecoilRoot>
+      <ThemeProvider theme={theme}>
+        <BrowserRouter>
+          <GlobalStyle />
+          <App />
+        </BrowserRouter>
+      </ThemeProvider>
+    </RecoilRoot>
+  </QueryClientProvider>
 );
