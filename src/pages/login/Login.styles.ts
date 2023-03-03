e9dd2a8ccc460c5e35bd706338e838fabb85f8a8
shgus1224 <shgus1224@naver.com>
2023-03-04 07:40:39 +0900

    style: 로그인 페이지 백그라운드 크기 수정

--- src/pages/login/Login.styles.ts
+++ src/pages/login/Login.styles.ts
@@ -5,6 +5,8 @@ import loginBack from '../../assets/images/loginBack.png';
 export const LoginComponent = styled.div`
   display: flex;
   flex-direction: column;
+  width: 100%;
+  max-width: 412px;
 `;
 
 export const HeaderText = styled.div`
@@ -12,7 +14,7 @@ export const HeaderText = styled.div`
 `;
 
 export const Input = styled.input`
-  width: 335px;
+  width: 100%;
   height: 50px;
   margin-bottom: 10px;
 
@@ -23,19 +25,20 @@ export const Input = styled.input`
 `;
 
 export const MainContainer = styled.div`
-  width: 375px;
-  max-width: 375px;
+  width: 100%;
+  max-width: 412px;
   position: relative;
   left: 0;
   right: 0;
   height: 812px;
   background-image: url(${loginBack});
+  background-size: cover;
   margin: 0 auto;
 `;
 
 export const FormContainer = styled.div`
-  width: 375px;
-  max-width: 375px;
+  width: 100%;
+  max-width: 412px;
   height: 405px;
   position: absolute;
   /* top: 359px; */
@@ -43,24 +46,29 @@ export const FormContainer = styled.div`
   background-color: white;
   border-top-left-radius: 20px;
   border-top-right-radius: 20px;
-  padding-left: 21px;
   padding-top: 10px;
 `;
 
 export const HeaderContainer = styled.div`
   position: absolute;
   top: 25px;
+  left: 20px;
 `;
 
 export const InputContnainer = styled.div`
   position: absolute;
   top: 77px;
-  width: 335px;
+  width: 100%;
+  max-width: 412px;
+  padding-right: 20px;
+  padding-left: 20px;
+
+  margin: 0 auto;
   height: 79px;
 `;
 
 export const NameInput = styled.input<{ isPasswordError: boolean }>`
-  width: 335px;
+  width: 100%;
   height: 50px;
   border: 1px solid ${theme.colors.gray04};
   border-radius: 5px;
@@ -76,7 +84,7 @@ export const NameInput = styled.input<{ isPasswordError: boolean }>`
 `;
 
 export const PasswordInput = styled.input<{ isPasswordError: boolean }>`
-  width: 335px;
+  width: 100%;
   height: 50px;
   border: 1px solid
     ${(props) => (props.isPasswordError ? '#ED7C55' : theme.colors.gray04)};
@@ -98,7 +106,8 @@ export const CheckBoxContainer = styled.div`
   justify-content: space-between;
   ${theme.typography.medium04}
   top: 197px;
-  width: 335px;
+  width: 100%;
+  right: 20px;
 `;
 
 export const RightWrapper = styled.div`
