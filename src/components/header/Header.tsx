ac8f61deea55d7e8f565d8ca5849afe0b2a8965e
shgus1224 <shgus1224@naver.com>
2023-03-04 08:01:06 +0900

    feat: 헤더 상단 공유버튼 클릭시 클립보드 복사

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -7,9 +7,13 @@ import emailRegistered from '../../assets/icons/emailRegistered.svg';
 import room from '../../assets/data/room.json';
 import { useRecoilState } from 'recoil';
 import { emailState } from '../../atoms/emailAtoms';
+import CopyToClipboard from 'react-copy-to-clipboard';
+import { useState } from 'react';
+import { useParams } from 'react-router-dom';
 
 const Header = ({ pageName }: { pageName: string }) => {
   const [isEmailRegistered, setIsEmailRegistered] = useRecoilState(emailState);
+  const { roomUuid } = useParams();
 
   return (
     <Wrapper>
@@ -22,7 +26,12 @@ const Header = ({ pageName }: { pageName: string }) => {
           <Icon src={emailDefault} alt="email default" />
         )
       ) : (
-        <Icon src={share} alt="share" />
+        <CopyToClipboard
+          text={`https://modutime.site/Current/${roomUuid}`}
+          onCopy={() => alert('클립보드에 복사되었습니다.')}
+        >
+          <Icon src={share} alt="share" />
+        </CopyToClipboard>
       )}
     </Wrapper>
   );
