32177791f7a8fe4c7b6ba0b4cbdcf0f551fd0b2a
Joohyun Kim <corinthionia@gmail.com>
2023-03-03 22:35:21 +0900

    feat: 조율 결과 페이지에 필요한 컴포넌트 구현

--- src/pages/result/Result.tsx
+++ src/pages/result/Result.tsx
@@ -2,9 +2,17 @@ import styled from '@emotion/styled';
 import Header from '../../components/header/Header';
 import SelectBox from '../../components/selectBox/SelectBox';
 import theme from '../../styles/theme';
+import { css } from '@emotion/react';
 
 import nobody from '../../assets/images/nobody.png';
+import Accordion from '../../components/accordion/Accordion';
+import { useState } from 'react';
 const Result = () => {
+  const [isConfirmed, setIsConfirmed] = useState(false);
+
+  const handleConfirmButtonClick = () => {
+    setIsConfirmed(!isConfirmed);
+  };
   return (
     <Wrapper>
       <Header />
@@ -27,13 +35,23 @@ const Result = () => {
             <NobodyText>모두가 되는 시간이 없어요</NobodyText>
           </Nobody>
         </NobodyWrapper>
+        <Accordion />
+        <Accordion />
+        <TimeWrapper isConfirmed={isConfirmed}>
+          2월 11일 (월) 18:00 ~ 20:00
+          <ConfirmButton
+            isConfirmed={isConfirmed}
+            onClick={handleConfirmButtonClick}
+          >
+            {isConfirmed ? '확정 취소' : '확정'}
+          </ConfirmButton>
+        </TimeWrapper>
       </Body>
     </Wrapper>
   );
 };
 
 export const Wrapper = styled.div`
-  border: 1px solid red;
   width: 100%;
   max-width: 375px;
   margin: 0 auto;
@@ -105,6 +123,47 @@ const NobodyRabbit = styled.img`
 const NobodyText = styled.span`
   color: ${theme.colors.gray04};
   ${theme.typography.semibold03};
+`;
+
+const TimeWrapper = styled.div<{ isConfirmed: boolean }>`
+  width: 100%;
+  height: 64px;
+  padding: 0 20px;
+  margin-top: 10px;
+
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  background: ${({ isConfirmed }) =>
+    isConfirmed ? `${theme.colors.purple06}` : `${theme.colors.purple01}`};
+  color: ${({ isConfirmed }) =>
+    isConfirmed ? `${theme.colors.gray01}` : `${theme.colors.gray06}`};
+
+  border-radius: 7px;
+  ${theme.typography.semibold03};
+`;
+
+const ConfirmButton = styled.div<{ isConfirmed: boolean }>`
+  height: 28px;
+  padding: 0 8px;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  border-radius: 4px;
+  ${theme.typography.medium04};
+
+  color: ${({ isConfirmed }) =>
+    isConfirmed ? `${theme.colors.gray01}` : `${theme.colors.purple06}`};
+
+  border: ${({ isConfirmed }) =>
+    isConfirmed
+      ? `1px solid ${theme.colors.gray01}`
+      : `1px solid ${theme.colors.purple06}`};
+
+  cursor: pointer;
 `;
 
 export default Result;
