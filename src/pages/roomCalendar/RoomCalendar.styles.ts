4214601d4fe8c965df16f0e42b443d2476186f76
shgus1224 <shgus1224@naver.com>
2023-05-13 22:00:28 +0900

    fix: 캘린더 페이지 컴포넌트 width 통일

--- src/pages/roomCalendar/RoomCalendar.styles.ts
+++ src/pages/roomCalendar/RoomCalendar.styles.ts
@@ -2,10 +2,10 @@ import styled from '@emotion/styled';
 import theme from '../../styles/theme';
 
 export const MainContainer = styled.div`
-  width: 100%;
-  height: 812px;
-  max-width: 412px;
   position: relative;
+  width: 100%;
+  max-width: 412px;
+  height: 812px;
   left: 0;
   right: 0;
   background-color: ${theme.colors.gray01};
@@ -16,21 +16,22 @@ export const MainContainer = styled.div`
 export const HeaderContainer = styled.div`
   position: absolute;
   top: 33px;
-  left: 20px;
   width: 100%;
   height: 116px;
+  left: 20px;
 `;
 
 export const Line = styled.img`
   position: absolute;
   top: 482px;
   width: 100%;
+  padding-inline: 20px;
 `;
 
 export const TimePickerContainer = styled.div`
   position: absolute;
-  width: 100%;
   top: 495px;
+  width: 100%;
   height: 135px;
   z-index: 1;
 `;
@@ -44,26 +45,28 @@ export const TimePickerWrapper = styled.div`
 
 export const GreyBox = styled.div`
   position: absolute;
-  width: 100%;
+  top: 42px;
+  width: 90%;
   height: 48px;
   z-index: 2;
+  margin-inline: 20px;
+  border-radius: 4px;
   background-color: ${theme.colors.gray02};
-  top: 42px;
 `;
 
 export const DependingBox = styled.div`
   position: absolute;
-  z-index: 4;
-  background-color: rgba(256, 256, 256, 0.6);
   width: 100%;
   height: 130px;
+  z-index: 4;
+  background-color: rgba(256, 256, 256, 0.6);
 `;
 
 export const CheckBoxContainer = styled.div`
   position: absolute;
-  top: 625px;
   width: 100%;
   height: 152px;
+  top: 625px;
   padding: 20px;
   padding-bottom: 50px;
 `;
