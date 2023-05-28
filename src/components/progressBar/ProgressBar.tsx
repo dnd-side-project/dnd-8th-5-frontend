439b421233c37cc45c651e3886dba552dce9e5f8
Joohyun Kim <corinthionia@gmail.com>
2023-05-29 02:25:48 +0900

    feat: 조율 현황 페이지 프로그레스 바 구현

--- src/components/progressBar/ProgressBar.tsx
+++ src/components/progressBar/ProgressBar.tsx
@@ -1,26 +1,77 @@
 import {
   Bar,
+  BarWrapper,
   Proportion,
+  Rabbit,
   RabbitWrapper,
   Span,
   Wrapper,
 } from './progressBar.styles';
+import { useEffect, useRef, useState } from 'react';
+
 import { currentParticipants } from '../../types/roomInfo';
 
+import rabbit0029 from '../../assets/images/rabbit0029.png';
+import rabbit100 from '../../assets/images/rabbit100.png';
+import rabbit6199 from '../../assets/images/rabbit6199.png';
+import rabbit3060 from '../../assets/images/rabbit3060.png';
+
 const ProgressBar = ({ headCount, participants }: currentParticipants) => {
+  const progress = (headCount && participants.length / headCount) || 0;
+
+  const wrapperRef = useRef<HTMLDivElement>(null);
+  const barRef = useRef<HTMLDivElement>(null);
+  const rabbitRef = useRef<HTMLImageElement>(null);
+
+  const [position, setPosition] = useState<number>(20);
+
+  useEffect(() => {
+    const wrapperWidth = (wrapperRef.current as HTMLDivElement).offsetWidth;
+    const barRect = barRef.current?.getBoundingClientRect().right;
+
+    console.log(barRect, barRef.current?.offsetWidth, wrapperWidth);
+
+    if (barRef.current && barRect) {
+      if (barRect < wrapperWidth) {
+        setPosition(barRef.current.offsetWidth);
+      } else {
+        setPosition(wrapperWidth - 40);
+      }
+    }
+  }, [barRef]);
+
   return (
-    <>
+    <Wrapper ref={wrapperRef}>
       <RabbitWrapper>
+        {
+          <Rabbit
+            ref={rabbitRef}
+            src={
+              progress === 1
+                ? rabbit100
+                : progress > 0.6
+                ? rabbit6199
+                : progress > 0.3
+                ? rabbit3060
+                : rabbit0029
+            }
+            alt="rabbit100"
+            style={{ left: `${position - 36}px` }}
+          />
+        }
         <Proportion>
           <Span>{participants.length}</Span> / {headCount}
         </Proportion>
       </RabbitWrapper>
-      <Wrapper>
-        {headCount && (
-          <Bar headCount={headCount} participantsNumber={participants.length} />
-        )}
-      </Wrapper>
-    </>
+
+      <BarWrapper>
+        <Bar
+          ref={barRef}
+          headCount={headCount || 0}
+          participantsNumber={participants.length}
+        />
+      </BarWrapper>
+    </Wrapper>
   );
 };
 
--- src/components/progressBar/progressBar.styles.ts
+++ src/components/progressBar/progressBar.styles.ts
@@ -3,6 +3,12 @@ import theme from '../../styles/theme';
 
 export const Wrapper = styled.div`
   width: 100%;
+  height: 85px;
+  margin-top: 16px;
+`;
+
+export const BarWrapper = styled.div`
+  width: 100%;
   max-width: 412px;
   height: 17px;
   border-radius: 61px;
@@ -16,6 +22,8 @@ export const Bar = styled.div<{
   width: ${({ headCount, participantsNumber }) =>
     headCount ? `${(participantsNumber / headCount) * 100}%` : 0};
   height: 100%;
+  margin-top: 4px;
+
   background: linear-gradient(270deg, #6a7bff 1.48%, #cad0ff 100%);
   border-radius: 61px;
 
@@ -34,10 +42,9 @@ export const Bar = styled.div<{
 
 export const RabbitWrapper = styled.div`
   width: 100%;
-  max-width: 412px;
-  margin-top: 34px;
 
   position: relative;
+
   display: flex;
   align-items: flex-end;
 `;
@@ -60,12 +67,12 @@ export const Span = styled.span`
   ${theme.typography.medium05};
 `;
 
-export const Rabbit = styled.img<{ position: number }>`
-  height: 54px;
-  position: relative;
-  /* left: ${({ position }) => `${position * 335}px`}; */
-  left: 0;
-  bottom: 0;
+export const Rabbit = styled.img`
+  width: 54px;
+  height: 63px;
 
+  position: relative;
   z-index: 2;
+
+  filter: drop-shadow(0px 0px 11px rgba(106, 123, 255, 0.49));
 `;
