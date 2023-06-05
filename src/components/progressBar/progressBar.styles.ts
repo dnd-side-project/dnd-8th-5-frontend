8c5d3ea81759ac476cd4135944582c96fd9c2642
Joohyun Kim <corinthionia@gmail.com>
2023-06-05 15:08:31 +0900

    fix: 조율 현황 Progress bar의 토끼 위치 수정 (#74)
    * feat: 조율 현황 페이지 프로그레스 바 구현
    
    * feat: 프로그레스 바 구현
    
    * fix: 프로그레스 바 위치 수정
    
    * fix: 프로그레스 바 위 토끼 위치 수정

--- src/components/progressBar/progressBar.styles.ts
+++ src/components/progressBar/progressBar.styles.ts
@@ -3,47 +3,60 @@ import theme from '../../styles/theme';
 
 export const Wrapper = styled.div`
   width: 100%;
+  height: 85px;
+  margin-top: 16px;
+`;
+
+export const BarWrapper = styled.div`
+  width: 100%;
   max-width: 412px;
   height: 17px;
   border-radius: 61px;
   background: ${theme.colors.gray02};
 `;
 
-export const Bar = styled.div<{
+export const BarCover = styled.div<{
   headCount: number;
   participantsNumber: number;
 }>`
   width: ${({ headCount, participantsNumber }) =>
     headCount ? `${(participantsNumber / headCount) * 100}%` : 0};
   height: 100%;
-  background: linear-gradient(270deg, #6a7bff 1.48%, #cad0ff 100%);
+  margin-top: 5px;
+
+  border-radius: 61px;
+  overflow: hidden;
+`;
+
+export const Bar = styled.div`
+  width: 100%;
+  height: 100%;
   border-radius: 61px;
 
-  animation: progressBar 1s ease-out;
+  background: linear-gradient(270deg, #6a7bff 1.48%, #cad0ff 100%);
 
+  animation: progressBar 1s ease-out;
   @keyframes progressBar {
     0% {
       width: 0%;
     }
     100% {
-      width: ${({ headCount, participantsNumber }) =>
-        headCount ? `${(participantsNumber / headCount) * 100}%` : 0};
+      width: 100%;
     }
   }
 `;
 
 export const RabbitWrapper = styled.div`
   width: 100%;
-  max-width: 412px;
-  margin-top: 34px;
 
   position: relative;
+
   display: flex;
   align-items: flex-end;
 `;
 
 export const Proportion = styled.div`
-  margin-bottom: 4px;
+  margin-bottom: 0px;
   margin-left: auto;
 
   color: ${theme.colors.gray05};
@@ -60,12 +73,23 @@ export const Span = styled.span`
   ${theme.typography.medium05};
 `;
 
-export const Rabbit = styled.img<{ position: number }>`
-  height: 54px;
-  position: relative;
-  /* left: ${({ position }) => `${position * 335}px`}; */
-  left: 0;
-  bottom: 0;
+export const Rabbit = styled.img<{ leftPosition: number }>`
+  width: 54px;
+  height: 63px;
 
-  z-index: 2;
+  position: relative;
+
+  left: ${({ leftPosition }) => `${leftPosition}px`};
+  filter: drop-shadow(0px 0px 11px rgba(106, 123, 255, 0.49));
+
+  animation: rabbitPosition 1s ease-out;
+
+  @keyframes rabbitPosition {
+    0% {
+      left: 0px;
+    }
+    100% {
+      left: ${({ leftPosition }) => `${leftPosition}px`};
+    }
+  }
 `;
