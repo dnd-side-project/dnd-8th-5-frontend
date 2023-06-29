65f691c8dda82b78f83bef0b78df1cd44c0a84fd
Joohyun Kim <corinthionia@gmail.com>
2023-06-30 00:39:09 +0900

    feat: 방 생성 시 24시 추가

--- src/utils/getRange.ts
+++ src/utils/getRange.ts
@@ -1,10 +1,10 @@
-export const getRange = (start: number, end: number) => {
+export const getRange = (start: number, end: number, step = 1) => {
   const arr = [];
   const length = end - start - 1;
 
   for (let i = 0; i <= length; i++) {
     arr[i] = start;
-    start++;
+    start += step;
   }
 
   return arr;
