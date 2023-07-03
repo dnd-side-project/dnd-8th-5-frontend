f4c0ab8af802ff9543a65c0e7a643a5b4d82f425
shgus1224 <shgus1224@naver.com>
2023-07-03 21:26:29 +0900

    fix: 초대페이지 등록 현황 보러가기 버튼 height 설정

--- src/pages/invite/Invite.styles.ts
+++ src/pages/invite/Invite.styles.ts
@@ -139,6 +139,7 @@ export const BottomButton = styled.button`
 export const BottomSubBuuton = styled.button`
   width: calc(100% - 40px);
   max-width: 375px;
+  height: 52px;
   margin: 0 auto;
   position: absolute;
   border-radius: 6px;
@@ -148,5 +149,5 @@ export const BottomSubBuuton = styled.button`
   align-items: center;
   justify-content: center;
   position: absolute;
-  top: 708px;
+  top: 690px;
 `;
