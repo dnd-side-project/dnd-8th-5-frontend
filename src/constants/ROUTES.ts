818af620de703bea0c0b06535f9c49e954cdc218
shgus1224 <shgus1224@naver.com>
2023-08-05 23:10:35 +0900

    fix: ROUTES 방생선 관련 상수 추가 및 addTime 값 오류 수정

--- src/constants/ROUTES.ts
+++ src/constants/ROUTES.ts
@@ -2,6 +2,10 @@ export const ROUTES = {
   LANDING: '/',
   INVITE: '/invite',
   CURRENT: '/current',
-  ADD_TIME: '/addTime',
+  ADD_TIME: '/add',
   RESULT: '/result',
+  LOGIN: '/login',
+  ROOM_START: '/roomStart',
+  ROOM_CALENDAR: '/roomCalendar',
+  ROOM_TIMER: '/roomTimer',
 };
