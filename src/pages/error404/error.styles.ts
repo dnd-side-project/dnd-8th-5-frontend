400bede93cca3488fd4ecd4963e93bcbf7eb52f2
shgus1224 <shgus1224@naver.com>
2023-08-05 16:45:40 +0900

    chore: 에러 페이지 스타일 파일추가

---
+++ src/pages/error404/error.styles.ts
@@ -0,0 +1,22 @@
+import styled from '@emotion/styled';
+import background from '../../assets/images/errorBack.webp';
+
+export const MainContainer = styled.div`
+  width: 100%;
+  max-width: 412px;
+  height: 100vh;
+
+  margin: 0 auto;
+  background-image: url(${background});
+  background-size: cover;
+`;
+
+export const BottomButton = styled.div`
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  height: 90px;
+  background-color: ${(props) => props.theme.colors.gray01};
+  display: flex;
+`;
