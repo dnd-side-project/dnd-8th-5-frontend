d81b37b075b177a12c50f7481e2e5be15f7ed289
Joohyun Kim <corinthionia@gmail.com>
2025-03-30 17:09:22 +0900

    feat: 로그인 페이지 디자인 변경

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,39 +1,40 @@
-import { useCallback, useEffect, useState, useRef, FormEvent } from 'react';
+import { useEffect, useState, useRef, FormEvent } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
-import loginBg from '@/assets/images/loginBack.webp';
+import * as Sentry from '@sentry/react';
 
 import {
   FormContainer,
   HeaderText,
-  LoginComponent,
   MainContainer,
-  NameInput,
-  PasswordInput,
-  CheckBoxContainer,
-  PasswordError,
-  RightWrapper,
-  ImgWrapper,
-  TextWrapper,
+  Input,
   Logo,
+  FormWrapper,
+  LoginButton,
+  BorderWrapper,
+  Border,
+  KakaoLoginButton,
+  ErrorMessage,
+  InputWrapper,
 } from './index.styles';
-import BottomButton from '@/components/commons/bottomButton';
 
 import useInputs from '@/hooks/useFormInput';
 import useInputScroll from '@/hooks/useInputScroll';
 
 import { ROUTES } from '@/constants/ROUTES';
-import checkedBox from '@/assets/icons/checkedBox.png';
-import uncheckedbox from '@/assets/icons/uncheckdBox.png';
 
 import { useGetRoomInfo } from '@/queries/room/useGetRoomInfo';
 import { usePostUserInfo } from '@/queries/auth/usePostUserInfo';
 import { Layout } from '@/components/commons/layout';
+
+import loginBg from '@/assets/images/login_bg.webp';
+import kakao from '@/assets/icons/kakao.svg';
 
 const Login = () => {
   const { roomUUID } = useParams() as { roomUUID: string };
   const { data: room } = useGetRoomInfo(roomUUID);
 
   const [saveUserInfo, setSaveUserInfo] = useState<boolean>(false);
+  const [isNameError, setIsNameError] = useState<boolean>(false);
   const [isPasswordError, setIsPasswordError] = useState<boolean>(false);
 
   const inputNameRef = useRef<HTMLInputElement>(null);
@@ -47,10 +48,6 @@ const Login = () => {
     password: '',
   });
 
-  const onClickSaveUserInfo = useCallback(() => {
-    setSaveUserInfo((prev) => !prev);
-  }, [saveUserInfo]);
-
   const canGoNext = form.name && form.password.length === 4 ? true : false;
 
   const navigate = useNavigate();
@@ -62,6 +59,12 @@ const Login = () => {
 
     if (Number.isNaN(Number(form.password))) {
       alert('비밀번호는 숫자만 입력해주세요');
+      return;
+    }
+
+    if (saveUserInfo) {
+      Sentry.captureMessage(`Save user info`);
+      alert('비밀번호는 숫자만 입력해 주세요');
       return;
     }
 
@@ -90,56 +93,54 @@ const Login = () => {
     <Layout>
       <MainContainer>
         <Logo src={loginBg} />
-        <FormContainer onSubmit={handleFormSubmit}>
-          <HeaderText>{room?.title}</HeaderText>
-          <LoginComponent>
-            <NameInput
+        <FormContainer>
+          <HeaderText>{room?.title?.slice(0, 15)}</HeaderText>
+          <FormWrapper onSubmit={handleFormSubmit}>
+            <Input
               autoComplete="off"
               ref={inputNameRef}
               type="text"
               name="name"
-              placeholder="이름을 입력하세요"
-              maxLength={4}
+              placeholder="이름 입력"
+              maxLength={5}
               value={form.name}
               onChange={onChangeForm}
-              isPasswordError={isPasswordError}
+              isError={isNameError}
             />
-            <PasswordInput
-              autoComplete="off"
-              ref={inputPasswordRef}
-              type="password"
-              pattern="[0-9]*"
-              inputMode="numeric"
-              name="password"
-              placeholder="비밀번호 4자리를 설정하세요"
-              value={form.password}
-              onChange={onChangeForm}
-              maxLength={4}
-              isPasswordError={isPasswordError}
-            />
-          </LoginComponent>
-          <CheckBoxContainer>
-            {isPasswordError ? (
-              <PasswordError>비밀번호가 일치하지 않아요</PasswordError>
-            ) : (
-              <div />
-            )}
-            <RightWrapper
-              id="save-user-info-button"
-              onClick={onClickSaveUserInfo}
-            >
-              <ImgWrapper>
-                <img src={saveUserInfo ? checkedBox : uncheckedbox} />
-              </ImgWrapper>
-              <TextWrapper>정보 저장</TextWrapper>
-            </RightWrapper>
-          </CheckBoxContainer>
-          <BottomButton
-            id="login-button"
-            type="submit"
-            text={'로그인'}
-            isActivated={canGoNext}
-          />
+            <InputWrapper>
+              <Input
+                autoComplete="off"
+                ref={inputPasswordRef}
+                type="password"
+                pattern="[0-9]*"
+                inputMode="numeric"
+                name="password"
+                placeholder="4자리 비밀번호 입력"
+                value={form.password}
+                onChange={onChangeForm}
+                maxLength={4}
+                isError={isPasswordError}
+              />
+              {isNameError && (
+                <ErrorMessage>이름을 4자리 이내로 입력해 주세요</ErrorMessage>
+              )}
+              {isPasswordError && (
+                <ErrorMessage>비밀번호가 일치하지 않아요</ErrorMessage>
+              )}
+            </InputWrapper>
+            <LoginButton type="submit">로그인</LoginButton>
+          </FormWrapper>
+
+          <BorderWrapper>
+            <Border />
+            <span>또는</span>
+            <Border />
+          </BorderWrapper>
+
+          <KakaoLoginButton>
+            <img src={kakao} alt="카카오 로고" />
+            카카오 로그인
+          </KakaoLoginButton>
         </FormContainer>
       </MainContainer>
     </Layout>
