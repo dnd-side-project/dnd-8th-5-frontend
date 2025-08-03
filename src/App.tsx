68a4fa3776173e9ccfadb2a86e79d6aa8d28abef
Joohyun Kim <corinthionia@gmail.com>
2025-08-03 14:10:00 +0900

    feat: 페이지 메타 태그 개선

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from 'react-router-dom';
+import { HelmetProvider } from 'react-helmet-async';
 import { ROUTES } from './constants/ROUTES';
-
+import Landing from './pages/landing';
 import AddTime from './pages/addTime';
 import Current from './pages/current';
 import RoomCalendar from './pages/roomCalendar';
@@ -13,26 +14,26 @@ import Error from './pages/404';
 import useScrollToTop from './hooks/useScrollToTop';
 import useGoogleAnalytics from './hooks/useGoogleAnalytics';
 
-import Landing from './pages/landing';
-
 function App() {
   useGoogleAnalytics();
   useScrollToTop();
 
   return (
-    <Routes>
-      <Route path="/" element={<Landing />} />
-      <Route path={`${ROUTES.ROOM_START}`} element={<RoomStart />} />
-      <Route path={`${ROUTES.ROOM_CALENDAR}`} element={<RoomCalendar />} />
-      <Route path={`${ROUTES.ROOM_TIMER}`} element={<Timer />} />
-      <Route path={`${ROUTES.INVITE}/:roomUUID`} element={<Invite />} />
-      <Route path={`${ROUTES.LOGIN}/:roomUUID`} element={<Login />} />
-      <Route path={`${ROUTES.CURRENT}/:roomUUID`} element={<Current />} />
-      <Route path={`${ROUTES.ADD_TIME}/:roomUUID`} element={<AddTime />} />
-      <Route path={`${ROUTES.RESULT}/:roomUUID`} element={<Result />} />
-      <Route path={`${ROUTES.ERROR}`} element={<Error />} />
-      <Route path="*" element={<Error />} />
-    </Routes>
+    <HelmetProvider>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path={`${ROUTES.ROOM_START}`} element={<RoomStart />} />
+        <Route path={`${ROUTES.ROOM_CALENDAR}`} element={<RoomCalendar />} />
+        <Route path={`${ROUTES.ROOM_TIMER}`} element={<Timer />} />
+        <Route path={`${ROUTES.INVITE}/:roomUUID`} element={<Invite />} />
+        <Route path={`${ROUTES.LOGIN}/:roomUUID`} element={<Login />} />
+        <Route path={`${ROUTES.CURRENT}/:roomUUID`} element={<Current />} />
+        <Route path={`${ROUTES.ADD_TIME}/:roomUUID`} element={<AddTime />} />
+        <Route path={`${ROUTES.RESULT}/:roomUUID`} element={<Result />} />
+        <Route path={`${ROUTES.ERROR}`} element={<Error />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+    </HelmetProvider>
   );
 }
 
