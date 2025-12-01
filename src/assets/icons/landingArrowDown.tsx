72237f79e8ecd379642acea5402849775bc9b4d1
Joohyun Kim <corinthionia@gmail.com>
2025-12-02 00:42:26 +0900

    feat: 날짜 분기 처리

---
+++ src/assets/icons/landingArrowDown.tsx
@@ -0,0 +1,34 @@
+export function LandingArrowDownIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="30"
+      height="30"
+      fill="none"
+      viewBox="0 0 30 30"
+    >
+      <path
+        fill="url(#paint0_linear_1733_11628)"
+        d="M8.572 0h12.857v20.455H8.572z"
+      />
+      <path
+        fill="#fff"
+        d="M15.715 29.268a1 1 0 0 1-1.43 0L3.669 18.403c-.618-.633-.17-1.698.716-1.698h21.23c.886 0 1.334 1.065.716 1.698z"
+      />
+      <defs>
+        <linearGradient
+          id="paint0_linear_1733_11628"
+          x1="15.001"
+          x2="15.001"
+          y1="0"
+          y2="10.909"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stopColor="#fff" />
+          <stop offset="0" stopColor="#fff" stopOpacity="0" />
+          <stop offset="1" stopColor="#fff" stopOpacity="0.938" />
+        </linearGradient>
+      </defs>
+    </svg>
+  );
+}
