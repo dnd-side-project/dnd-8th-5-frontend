44459b44f3055c00b1360df3704716ab9f5e609f
Joohyun Kim <corinthionia@gmail.com>
2025-12-03 23:51:28 +0900

    feat: 초대 페이지 디자인 수정

--- src/pages/invite/index.tsx
+++ src/pages/invite/index.tsx
@@ -13,17 +13,25 @@ import {
   LowerBoxWrapper,
   RoomTitle,
   ParticipantsTitle,
-  ParticipantsWraaper,
+  ParticipantsWrapper,
   Participant,
   BottomSubButton,
 } from './index.styles';
-import calendar from '@/assets/images/calendar.png';
 import { useEffect } from 'react';
 import { useAuth } from '@/hooks/useAuth';
 import { roomState } from '@/atoms/roomAtom';
 import { useGetRoomInfo } from '@/queries/room/useGetRoomInfo';
 import { ROUTES } from '@/constants/ROUTES';
 import { Helmet } from 'react-helmet-async';
+
+import calendar from '@/assets/images/invite_calendar.webp';
+import calendarXmas from '@/assets/images/invite_calendar_xmas.webp';
+
+const isAfterXmas2025 = (() => {
+  const now = new Date();
+  const xmas2025 = new Date('2025-12-25T00:00:00'); // 로컬 시간 기준
+  return now >= xmas2025;
+})();
 
 const Invite = () => {
   const { roomUUID } = useParams() as { roomUUID: string };
@@ -46,12 +54,20 @@ const Invite = () => {
     for (let i = 0; i < room.participants.length; i++) {
       if (i > 6) {
         result.push(
-          <Participant key={room.participants[i].id}>+{overCount}</Participant>
+          <Participant
+            key={room.participants[i].id}
+            isAfterXmas2025={isAfterXmas2025}
+          >
+            +{overCount}
+          </Participant>
         );
         break;
       } else {
         result.push(
-          <Participant key={room.participants[i].id}>
+          <Participant
+            key={room.participants[i].id}
+            isAfterXmas2025={isAfterXmas2025}
+          >
             {room.participants[i].name}
           </Participant>
         );
@@ -86,21 +102,23 @@ const Invite = () => {
           content="쉽고 빠른 약속시간 정하기, 모두의 시간"
         />
       </Helmet>
-      <MainContainer>
+
+      <MainContainer isAfterXmas2025={isAfterXmas2025}>
         <HeaderWrapper>
-          <Calendar src={calendar} />
+          <Calendar src={isAfterXmas2025 ? calendar : calendarXmas} />
           <TitleWrapper>
             <Title>약속에 초대합니다</Title>
-            <SubTitle>지금 바로 약속시간을 조율해보세요!</SubTitle>
+            <SubTitle>지금 바로 약속 시간을 조율해 보세요!</SubTitle>
           </TitleWrapper>
         </HeaderWrapper>
+
         <BoxWrapper>
           <UpperBoxWrapper>
             <RoomTitle>{room.title}</RoomTitle>
           </UpperBoxWrapper>
           <LowerBoxWrapper>
             <ParticipantsTitle>참여자</ParticipantsTitle>
-            <ParticipantsWraaper>{getParticipant()}</ParticipantsWraaper>
+            <ParticipantsWrapper>{getParticipant()}</ParticipantsWrapper>
           </LowerBoxWrapper>
         </BoxWrapper>
 
@@ -108,7 +126,7 @@ const Invite = () => {
           일정 등록하기
         </BottomButton>
         <BottomSubButton onClick={handleSubButtonClick}>
-          등록 현황 보러가기
+          등록 현황 보러 가기
         </BottomSubButton>
       </MainContainer>
     </>
