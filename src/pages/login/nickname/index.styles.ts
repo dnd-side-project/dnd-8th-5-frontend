aa09f70c40d8bc949d5402b13f08aa33abbbd083
Joohyun Kim <corinthionia@gmail.com>
2026-02-18 14:11:28 +0900

    feat: 닉네임 입력 페이지 스타일링

--- src/pages/login/nickname/index.styles.ts
+++ src/pages/login/nickname/index.styles.ts
@@ -17,7 +17,7 @@ export const MainContainer = styled.div`
 
 export const Logo = styled.img`
   width: 100%;
-  height: 36%;
+  height: 40%;
   max-height: 333px;
   object-fit: cover;
   overscroll-behavior: none;
@@ -31,4 +31,86 @@ export const Body = styled.section`
   width: 100%;
   padding: 24px 20px 0;
   background: ${theme.colors.gray01};
+`;
+
+export const H1 = styled.h1`
+  margin: 0;
+  ${theme.typography.semibold01};
+`;
+
+export const Form = styled.form`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  gap: 6px;
+`;
+
+export const InputWrapper = styled.div`
+  display: flex;
+  width: 100%;
+  height: 54px;
+  min-height: 54px;
+  flex: 1;
+  gap: 12px;
+  align-items: center;
+  justify-content: space-between;
+  margin: 24px 0 0 0;
+  padding: 14px 16px;
+  border-radius: 6px;
+  border: 1px solid ${theme.colors.gray03};
+
+  div {
+    display: flex;
+    align-items: center;
+    gap: 3px;
+    width: 100%;
+  }
+
+  button {
+    padding: 3px 8px;
+    border-radius: 4px;
+    color: ${theme.colors.gray05};
+    background: ${theme.colors.gray025};
+    ${theme.typography.medium04};
+    flex-shrink: 0;
+  }
+
+  input {
+    width: 100%;
+    border: none;
+    outline: none;
+  }
+`;
+
+export const InputHelperText = styled.span`
+  color: ${theme.colors.gray04};
+  ${theme.typography.medium03};
+`;
+
+export const ErrorMessage = styled.span`
+  color: ${theme.colors.red02};
+  ${theme.typography.regular02};
+`;
+
+export const Bottom = styled.div`
+  position: fixed;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  padding: 10px 20px;
+  height: 90px;
+  background: ${theme.colors.gray01};
+  border-top: 1px solid ${theme.colors.gray02};
+
+  button {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+    height: 52px;
+    border-radius: 6px;
+    color: ${theme.colors.gray01};
+    background: ${theme.colors.purple06};
+    ${theme.typography.semibold03};
+  }
 `;
--- src/pages/login/nickname/index.tsx
+++ src/pages/login/nickname/index.tsx
@@ -1,25 +1,91 @@
+import { useState, useRef } from 'react';
 import { Layout } from '@/components/commons/layout';
 import roomStart from '@/assets/images/room_info_bg.png';
-import { Logo, MainContainer, Body } from './index.styles';
+import {
+  Logo,
+  MainContainer,
+  Body,
+  H1,
+  InputWrapper,
+  Form,
+  Bottom,
+  InputHelperText,
+  ErrorMessage,
+} from './index.styles';
 import { useParams } from 'react-router-dom';
 import { useGetRoomInfo } from '@/queries/room/useGetRoomInfo';
+
+const MOCK_NAME = '김주현';
 
 export default function LoginNickname() {
   const { roomId } = useParams() as { roomId: string };
   const { data: room } = useGetRoomInfo(roomId);
 
+  const [isEditing, setIsEditing] = useState(false);
+  const [nickname, setNickname] = useState(MOCK_NAME);
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const inputRef = useRef<HTMLInputElement>(null);
+
   if (!room) return;
+
+  const handleEditClick = () => {
+    setIsEditing(true);
+    setTimeout(() => inputRef.current?.focus(), 0);
+  };
+
+  const handleConfirmClick = () => {
+    if (nickname.length >= 1 && nickname.length <= 4) {
+      setIsEditing(false);
+    }
+  };
 
   return (
     <Layout>
       <MainContainer>
         <Logo src={roomStart} alt="room start logo" />
         <Body>
-          <h1>{room.title}</h1>
+          <H1>{room.title}</H1>
+          <Form>
+            <InputWrapper>
+              <div>
+                {isEditing ? (
+                  <input
+                    ref={inputRef}
+                    value={nickname}
+                    onChange={(e) => {
+                      if (e.target.value.length <= 4) {
+                        setNickname(e.target.value);
+                      }
+                    }}
+                    minLength={1}
+                    maxLength={4}
+                  />
+                ) : (
+                  <span>
+                    <strong>{nickname}</strong>
+                  </span>
+                )}
+                {!isEditing && <span>님으로 시작</span>}
+              </div>
+              {!isEditing && (
+                <button
+                  type="button"
+                  onClick={isEditing ? handleConfirmClick : handleEditClick}
+                >
+                  변경
+                </button>
+              )}
+              {isEditing && (
+                <InputHelperText>{`${nickname.length}/4`}</InputHelperText>
+              )}
+            </InputWrapper>
+            {!!errorMessage && <ErrorMessage>{errorMessage}</ErrorMessage>}
 
-          <form>
-            <input value="님으로 시작" />
-          </form>
+            <Bottom>
+              <button type="submit">시작하기</button>
+            </Bottom>
+          </Form>
         </Body>
       </MainContainer>
     </Layout>
